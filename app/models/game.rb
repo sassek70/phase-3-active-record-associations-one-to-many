0786709714468f99c2616496f692c4c7c7f99753
Kevin Sasse <sassek70@gmail.com>
2022-11-08 15:30:43 -0500

    commit tree 1dae190e3538177681739faa65c036cfb4a975f5

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,14 @@
+# class Game < ActiveRecord::Base
+#     #a game can have many reviews
+#     def reviews
+#         #self is the game instance
+#         Reviews.where(game_id: self.id)
+#     end
+# end
+
+
+#===============with ActiveRecord macros==============#
+
 class Game < ActiveRecord::Base
-  
+  has_many :reviews
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,13 @@
+# class Review < ActiveRecord::Base
+#     # a review belongs to a game
+#     def game
+#         #self is the review instance
+#         Game.find(self.game_id)
+#     end
+# end
+
+#===============with ActiveRecord macros==============#
+
 class Review < ActiveRecord::Base
-  
+   belongs_to :game
 end
